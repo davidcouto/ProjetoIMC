c945f6867fd7dbbca989ff2f38c8c74e77a8c06e
davidcouto <davidhcouto@gmail.com>
2014-11-04 19:56:02 -0200

    Model

--- avaliacoes/models.py
+++ avaliacoes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Avaliacao(models.Model):
+	nome = models.CharField(max_length=100)
+	sexo = models.CharField(max_length=20)
+	idade = models.IntegerField()
+	peso = models.DecimalField(max_digits=10, decimal_places=3)
+	altura = models.DecimalField(max_digits=10, decimal_places=3)
+	resultado = models.DecimalField(max_digits=10, decimal_places=3)
