1959d43d5e511903983612b2a5d75321cb634483
GuilhermeRodrigues <guilherme17_rodrigues@hotmail.com>
2014-11-04 21:27:35 -0200

    validar_cadastro e index

--- avaliacoes/views.py
+++ avaliacoes/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from avaliacoes.forms import AvaliacaoForm, LoginForm
+from django.contrib.auth.decorators import login_required
+from avaliacoes.models import Avaliacao, Usuario
 
-# Create your views here.
+
+
+def validar_cadastro(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            usuario = Usuario(
+                username=form.data['login'],
+                is_active=False)
+
+            usuario.set_password(form.data['senha'])
+            usuario.save()
